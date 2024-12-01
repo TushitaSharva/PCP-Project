f58cf710bc99a2514f199c463f5ebf6102b34272
TushitaSharva <tushitasharva03@gmail.com>
2024-12-01 10:35:31 +0530

    Sequential done

--- run.sh
+++ run.sh
@@ -5,8 +5,12 @@ if [ -n "$(ls -A "$outputs_directory")" ]; then
     rm "$outputs_directory"/*
 fi
 
+# input_file_name="inp.txt"
+# g++ -std=c++17 seq.cpp && ./a.out "$input_file_name"
+
 # To loop through all files in the subfolders of the 'testcases' directory and run all the test cases
 for input_file_name in ./inputs/*.txt; do
     g++ -std=c++17 seq.cpp && ./a.out "$input_file_name"
+    python3 python_Code.py "$input_file_name"
     echo "==================================================="
 done
--- seq.cpp
+++ seq.cpp
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <cstdlib>
 #include <ctime>
+#include <climits>
 #include <sstream>
 #include <mutex>
 #include <time.h>
@@ -15,80 +16,143 @@
 #include <atomic>
 #include <mutex>
 #include <thread>
+#include <queue>
+#include <stack>
 
 // Logger class, for printing and logging to log files and debugging
 class Logger
 {
 public:
-    template<typename... Args>
+    template <typename... Args>
     void DEBUG(Args... args)
     {
         std::ofstream debugfile("outputs/debug.txt", std::ios::app);
         std::ostringstream oss;
-        (oss << ... << args);  // Use fold expression to stream all arguments
+        (oss << ... << args); // Use fold expression to stream all arguments
         debugfile << oss.str() << "\n";
         debugfile.close();
     }
 
-    template<typename... Args>
+    template <typename... Args>
     void OUTPUT(Args... args)
     {
         std::ofstream outputfile("outputs/out.txt", std::ios::app);
         std::ostringstream oss;
-        (oss << ... << args);  // Stream all arguments
+        (oss << ... << args); // Stream all arguments
         outputfile << oss.str() << "\n";
         outputfile.close();
     }
 };
 
 // Function to convert time_point to a formatted string
-std::string getSysTime(const std::chrono::high_resolution_clock::time_point& tp)
+std::string getSysTime(const std::chrono::high_resolution_clock::time_point &tp)
 {
     auto time_t = std::chrono::high_resolution_clock::to_time_t(tp);
-    std::tm* tm = std::localtime(&time_t); // Convert to local time
+    std::tm *tm = std::localtime(&time_t); // Convert to local time
     char buffer[100];
     std::strftime(buffer, sizeof(buffer), "%H:%M:%S", tm);
     return std::string(buffer);
 }
 
 static Logger LOGGER;
-long n;
-std::vector<std::vector<long>> adjacencyList(n); 
+long V, E;
+std::vector<long> *adjacencyList;
 
 void readInput(std::string filename)
 {
     std::ifstream inputfile(filename);
 
-    inputfile >> n;
-    std::string str;
-    int number;
-    std::getline(inputfile, str); // to remove the first line
+    inputfile >> V >> E;
+    adjacencyList = new std::vector<long>[V];
 
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < E; i++)
     {
-        std::vector<long> neighbours;
-        std::getline(inputfile, str);
-        std::stringstream iss(str);
+        long u, v;
+        inputfile >> u >> v;
+        adjacencyList[u].push_back(v);
+    }
+    
+    inputfile.close();
+    return;
+}
 
-        while(iss >> number)
+void brandes()
+{
+    std::vector<double> CB(V, 0.0);
+
+    for(int s = 0; s < V; s++)
+    {
+        for(int v = 0; v < V; v++)
         {
-            neighbours.push_back(number);
-        }
+            std::vector<double> delta(V,0.0);
+            std::vector<std::vector<long>> prev(V);
+            std::vector<long> sigma(V, 0);
+            std::vector<long> dist(V, LLONG_MAX);
 
-        adjacencyList.push_back(neighbours);
+            sigma[s] = 1;
+            dist[s] = 0;
+
+            std::queue<long> queue; queue.push(s);
+            std::stack<long> stack;
+
+
+            while(!queue.empty())
+            {
+                long u = queue.front();
+                queue.pop();
+                stack.push(u);
+
+                for(auto v: adjacencyList[u])
+                {
+                    if(dist[v] == LLONG_MAX)
+                    {
+                        dist[v] = dist[u]+1;
+                        queue.push(v);
+                    }
+
+                    if(dist[v] == dist[u]+1)
+                    {
+                        sigma[v] = sigma[v] + sigma[u];
+                        prev[v].push_back(u);
+                    }
+                }
+            }
+
+            while(!stack.empty())
+            {
+                int v = stack.top();
+                stack.pop();
+                for(auto u : prev[v])
+                {
+                    delta[u] += (sigma[u] / sigma[v]) * (1.0 +delta[v]);
+
+                    if(v != s)
+                    {
+                        CB[v] = CB[v] + delta[v];
+                    }
+                }
+
+            }
+
+        }
     }
 
-    inputfile.close();
+    for(int i = 0; i < V; i++)
+    {
+        std::cout << CB[i]/V << " ";
+    }
+    std::cout << "\n";
     return;
 }
 
 int main(int argc, char *argv[])
 {
-    readInput(argv[0]);
+    readInput(argv[1]);
+
     auto start_time = std::chrono::high_resolution_clock::now();
     LOGGER.OUTPUT("The start time is ", getSysTime(start_time));
 
-    // Your code goes here
+    brandes();
 
     auto stop_time = std::chrono::high_resolution_clock::now();
     LOGGER.OUTPUT("The stop time is ", getSysTime(stop_time));
